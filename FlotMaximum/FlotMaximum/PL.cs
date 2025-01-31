7b21354fdc4565b62119d2adc243ea438b22cb0e
Sequoia <sequoiahumide@gmail.com>
2025-01-31 08:58:24 +0100

    creation systeme PL

--- FlotMaximum/FlotMaximum/PL.cs
+++ FlotMaximum/FlotMaximum/PL.cs
@@ -7,17 +7,84 @@ public class PL
 {
     public Solver solver;
     public Variable[] variables;
-    public Int32[] contraintes;
+    public Constraint[] constraints;
 
-    public PL(Int32 nbVariables, Int32[][] A, String symbol, Int32[] B)
+    public PL(int nbVariables, List<List<int>> A, List<String> symbol, List<int> B, List<int> obj, bool maximiser)
     {
+        solver = Solver.CreateSolver("SCIP");
+        
         variables = new Variable[nbVariables];
+        Int32 nbContrainte = B.Count;
+        constraints = new Constraint[nbContrainte];
         for (int i = 0; i < nbVariables; i++)
         {
             // Création de la variable x_i avec borne inférieure 0 et borne supérieure infinie
             variables[i] = solver.MakeNumVar(0.0, double.PositiveInfinity, $"x{i + 1}");
         }
-
+        
+        for (int i = 0; i < nbContrainte; i++)
+        {
+            if (symbol[i] == "<=")
+            {
+                constraints[i]= solver.MakeConstraint(double.NegativeInfinity, B[i]);
+            }
+            if (symbol[i] == "=")
+            {
+                constraints[i] = solver.MakeConstraint(B[i], B[i]);
+            }
+            if (symbol[i] == ">=")
+            {
+                constraints[i] = solver.MakeConstraint(B[i], double.PositiveInfinity);
+            }
+            
+            for (int j = 0; j < nbVariables; j++)
+            {
+                constraints[i].SetCoefficient(variables[j], A[i][j]);
+            }
+        }
+        
+        Objective objectif = solver.Objective();
+        for (int i = 0; i < variables.Length; i++)
+        {
+            objectif.SetCoefficient(variables[i], obj[i]);
+        }
+        if (maximiser)
+        {
+            objectif.SetMaximization();
+        }
+        else
+        {
+            objectif.SetMinimization();
+        }
     }
+    
+    public void Resoudre()
+    {
+        Solver.ResultStatus statut = solver.Solve();
+
+        if (statut == Solver.ResultStatus.OPTIMAL)
+        {
+            Console.WriteLine("Solution optimale trouvée !");
+            for (int i = 0; i < variables.Length; i++)
+            {
+                Console.WriteLine($"{variables[i].Name()} = {variables[i].SolutionValue()}");
+            }
+            Console.WriteLine($"Valeur optimale de la fonction objectif : {solver.Objective().Value()}");
+        }
+        else
+        {
+            Console.WriteLine("Pas de solution optimale trouvée.");
+        }
+    }
+
+    public PL(FlowNetwork flowNetwork)
+    {
+        solver = Solver.CreateSolver("SCIP");
+        variables = new Variable[10];
+        Int32 nbContrainte = 10;
+        constraints = new Constraint[nbContrainte];
+        
+    }
+
 
 }
--- FlotMaximum/FlotMaximum/Program.cs
+++ FlotMaximum/FlotMaximum/Program.cs
@@ -19,7 +19,7 @@ Vertex p = new Vertex("p");
 //FlowNetwork nf = new([
 //(a,d, 13), (a, b, 8), (a, c, 10), (b,c,26), (c,d,20),
 //(c,e,8),(c,f,24),(d,e,1),(d,b,2)], s, p, [(a, 38), (b, 1), (f, 2)], [(d, 7), (e, 7), (c, 1), (f, 27)]);
-RandomFlowNetwork randomFlow = new(1000, 10000);
+RandomFlowNetwork randomFlow = new(10, 15);
 FlowNetwork nf = randomFlow.Generate();
 Console.WriteLine("Généré avec " + nf.AdjVertices.Keys.Count + " sommets et " +
                   (nf.Edges.Count + nf.SourceNeighbors.Count + nf.PuitsNeighbors.Count) + " arêtes.");
@@ -27,3 +27,7 @@ var startTime = Stopwatch.GetTimestamp();
 var maxFlow = nf.EdmondsKarp();
 Console.WriteLine("Edmonds-Karp " + maxFlow.Value);
 Console.WriteLine("Elapsed : " + Stopwatch.GetElapsedTime(startTime));
+
+Console.WriteLine("\nOUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIS   : \n");
+PL newPL = new PL(2, [[5,3],[2,3],[1,3]], ["<=","<=","<="], [30,24,18], [8,6], true);
+newPL.Resoudre();

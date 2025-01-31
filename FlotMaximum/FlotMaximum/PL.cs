fdff2f556d8abacef111245d62e7537c3a81b802
Sequoia <sequoiahumide@gmail.com>
2025-01-31 09:50:38 +0100

    transfer

--- FlotMaximum/FlotMaximum/PL.cs
+++ FlotMaximum/FlotMaximum/PL.cs
@@ -14,7 +14,7 @@ public class PL
         solver = Solver.CreateSolver("SCIP");
         
         variables = new Variable[nbVariables];
-        Int32 nbContrainte = B.Count;
+        int nbContrainte = B.Count;
         constraints = new Constraint[nbContrainte];
         for (int i = 0; i < nbVariables; i++)
         {
@@ -80,9 +80,35 @@ public class PL
     public PL(FlowNetwork flowNetwork)
     {
         solver = Solver.CreateSolver("SCIP");
-        variables = new Variable[10];
-        Int32 nbContrainte = 10;
-        constraints = new Constraint[nbContrainte];
+        
+        int nbVariables = flowNetwork.Edges.Count;
+        int nbContraintes = flowNetwork.Edges.Count*2;
+        variables = new Variable[nbVariables];
+        constraints = new Constraint[nbContraintes];
+        
+
+        int var_i = 0;
+        foreach (var edge in flowNetwork.Edges)
+        {
+            var (u, v) = edge.Key; // u et v sont les sommets
+            int poids = edge.Value;
+            variables[var_i] = solver.MakeNumVar(0.0, double.PositiveInfinity, $"x_{u.ToString()}_{v.ToString()}");
+            var_i += 1;
+        }
+        
+        foreach (var edge in flowNetwork.Edges)
+        {
+            var (u, v) = edge.Key; // u et v sont les sommets
+            int poids = edge.Value;
+            constraints[i]= solver.MakeConstraint(double.NegativeInfinity, flowNetwork.Edges);
+            var_i += 1;
+        }
+        
+        for (int i = 0; i < nbVariables; i++)
+        {
+            constraints[i]= solver.MakeConstraint(double.NegativeInfinity, flowNetwork.Edges);
+            Constraint constraint = solver.MakeConstraint(3, 3);
+        }
         
     }
 

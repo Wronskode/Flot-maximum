8c0b9eda6164c4f16e3a48b03c3759e874c4a650
Julien <142849734+Wronskode@users.noreply.github.com>
2025-05-16 15:54:27 +0200

    refactor

---
+++ FlotMaximum/Curve.cs
@@ -0,0 +1,118 @@
+using System.Diagnostics;
+using ScottPlot;
+
+namespace FlotMaximum;
+
+public static class Curve
+{
+    public static void CreateCurves(
+        double density, int lowerBound, int upperBound, int step, int minNodes, int maxNodes,
+        List<(string name, Func<FlowNetwork, double> solver)> solvers)
+    {
+        List<double> xs = [];
+        Dictionary<string, List<double>> timings = new();
+        foreach (var (name, _) in solvers)
+        {
+            timings[name] = new List<double>();
+        }
+
+        Plot myPlot = new();
+
+        for (int i = minNodes; i <= maxNodes; i+=step)
+        {
+            Console.WriteLine("i = " + i);
+            RandomFlowNetwork randomFlow = new(i, density);
+            FlowNetwork nf = randomFlow.Generate(lowerBound, upperBound);
+            Console.WriteLine("Généré avec " + nf.AdjVertices.Keys.Count + " sommets et " + nf.Edges.Count + " arêtes.");
+            
+            xs.Add(i);
+
+            foreach (var (name, solver) in solvers)
+            {
+                Stopwatch sw = Stopwatch.StartNew();
+                var result = solver(nf);
+                sw.Stop();
+                timings[name].Add(sw.Elapsed.TotalSeconds);
+                Console.WriteLine($"{name} {result} in {sw.Elapsed.TotalSeconds}s");
+            }
+            
+            foreach (var (name2, values) in timings)
+            {
+                var curve = myPlot.Add.Scatter(xs, values);
+                curve.LegendText = name2;
+            }
+            
+            myPlot.Axes.Bottom.Label.Text = "Number of nodes";
+            myPlot.Axes.Left.Label.Text = "Seconds";
+            myPlot.ShowLegend(Alignment.UpperLeft);
+            myPlot.Legend.FontSize = 20;
+            myPlot.SavePng("../../../../courbes/benchmark"+Math.Round(density, 2)+".png", 1500, 1000);
+            myPlot = new Plot();
+        }
+    }
+
+    public static void CreateCurves(double density, string directoryPath, List<(string name, Func<FlowNetwork, double> solver)> solvers)
+    {
+        var di = new DirectoryInfo(directoryPath);
+        Dictionary<int, Dictionary<string, List<double>>> timingData = new();
+        List<double> xs = new();
+        var files = di.GetFiles().Where(file =>
+        {
+            string fileName = file.Name;
+            string densityStr = "";
+            int i = 1;
+            while (fileName[fileName.IndexOf('_') + i] != '_')
+            {
+                densityStr += fileName[fileName.IndexOf('_') + i];
+                i++;
+            }
+            return Math.Abs(Convert.ToDouble(densityStr) - density) < 1e-5;
+        }).OrderBy(file =>
+        {
+            FileFlowNetwork ffn = new(directoryPath + file.Name);
+            FlowNetwork nf = ffn.Generate();
+            return nf.AdjVertices.Count;
+        });
+        
+        foreach (var file in files)
+        {
+            FileFlowNetwork ffn = new(directoryPath + file.Name);
+            FlowNetwork nf = ffn.Generate();
+            int nodeCount = nf.AdjVertices.Count;
+            
+            if (!timingData.TryGetValue(nodeCount, out Dictionary<string, List<double>>? value))
+            {
+                value = new Dictionary<string, List<double>>();
+                timingData[nodeCount] = value;
+                foreach (var (name, _) in solvers)
+                {
+                    timingData[nodeCount][name] = new List<double>();
+                }
+                xs.Add(nodeCount);
+            }
+            
+            foreach (var (name, solver) in solvers)
+            {
+                var nf2 = (FlowNetwork)nf.Clone(); // Important si on modifie le réseau de flot
+                Stopwatch sw = Stopwatch.StartNew();
+                var result = solver(nf2);
+                sw.Stop();
+                value[name].Add(sw.Elapsed.TotalSeconds);
+                Console.WriteLine($"{name} {result} in {sw.Elapsed.TotalSeconds}s for {nodeCount} nodes");
+            }
+            Plot myPlot = new Plot();
+            foreach (var (name, _) in solvers)
+            {
+                List<double> avgTimes = timingData.Keys.Select(nc => timingData[nc][name].Average()).ToList();
+                var curve = myPlot.Add.Scatter(xs, avgTimes);
+                curve.LegendText = name;
+            }
+            myPlot.Axes.Bottom.Label.Text = "Number of nodes";
+            myPlot.Axes.Left.Label.Text = "Seconds";
+            myPlot.ShowLegend(Alignment.UpperLeft);
+            myPlot.Legend.FontSize = 20;
+            myPlot.Legend.FontName = Fonts.Serif;
+            myPlot.SavePng("../../../../courbes/benchmark" + Math.Round(density, 2) + ".png", 1500, 1000);
+        }
+    }
+}
---
+++ FlotMaximum/Program.cs
@@ -0,0 +1,84 @@
+﻿using FlotMaximum;
+
+
+// réseau de flot du TD de Bessy
+/*Vertex a = new Vertex("a");
+Vertex b = new Vertex("b");
+Vertex c = new Vertex("c");
+Vertex d = new Vertex("d");
+Vertex e = new Vertex("e");
+Vertex f = new Vertex("f");
+
+Vertex s = new Vertex("s");
+Vertex p = new Vertex("p");
+FlowNetwork nf = new([
+(a, b, 12), (a, c, 10), (d, b, 7), (b, c, 9), (c, a, 4), (c, d, 14)], s, p, [(a, 16), (c, 13)], [(b, 20), (d, 4)],
+[a,b,c,d]);*/
+
+//FlowNetwork nf = new([
+//(a,d, 13), (a, b, 8), (a, c, 10), (b,c,26), (c,d,20),
+//(c,e,8),(c,f,24),(d,e,1),(d,b,2)], s, p, [(a, 38), (b, 1), (f, 2)], [(d, 7), (e, 7), (c, 1), (f, 27)]);
+
+// var distances = nf.BFS_GetLevels();
+// foreach (KeyValuePair<Vertex, int> kvp in distances)
+// {
+//     Console.WriteLine($"Distance: {kvp.Key} - {kvp.Value}");
+// }
+// return;
+const string instancesPath = "../../../../Instances/";
+List<double> densities = [0.1, 0.5, 0.9];
+void DeleteInstances(string path)
+{
+    var di = new DirectoryInfo(path);
+    foreach (FileInfo file in di.GetFiles())
+    {
+        file.Delete(); 
+    }
+}
+
+void CreateInstances(string instancesPath, List<double> densities)
+{
+    DeleteInstances(instancesPath);
+
+    List<int> tailles = [10, 20, 50, 100, 200, 500, 1000];
+
+    for (int n = 30; n <= 200; n+=10)
+    {
+        foreach (double d in densities)
+        {
+            var i = 1;
+            while (i <= 3)
+            {
+                RandomFlowNetwork randomFlow = new(n, d);
+                FlowNetwork nf = randomFlow.Generate();
+                bool res = nf.IsConnected();
+                if (res)
+                {
+                    string fileName = instancesPath + $"inst{n}_{d}_{i}.txt";
+                    nf.CreateGraphWeightFile(fileName);
+                    Console.WriteLine("Le fichier a été créé avec succès.");
+                    i += 1;
+                }
+            }
+        }
+    }
+}
+
+//CreateInstances(instancesPath, densities);
+// Ligne Gurobi à commenter si vous n'avez pas de licence
+var solvers = new List<(string, Func<FlowNetwork, double>)>
+{
+    ("Ford-Fulkerson", nf => nf.FordFulkerson().Value),
+    ("Edmonds-Karp", nf => nf.EdmondsKarp().Value),
+    ("Dinic", nf => nf.Dinic().Value),
+    ("Poussage-Réétiquetage", nf => nf.Push_Label().Value),
+    ("Gurobi", nf => PL.SolveWithGurobi(nf)),
+    ("SCIP", nf => PL.SolveWithOrTools(nf, "SCIP")),
+    //("GLOP", nf => PL.SolveWithOrTools(nf, "GLOP")),
+    //("CP-SAT", nf => PL.SolveWithOrTools(nf, "CP-SAT")),
+};
+
+foreach (var density in densities)
+{
+    Curve.CreateCurves(density, instancesPath, solvers);
+}
